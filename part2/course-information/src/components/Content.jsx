15b2f532dca3ae46668561d2c903e3c5b92e93e9
Patryk Rogala <patryk@astarium.pl>
2024-10-27 21:16:27 +0100

    Exercises 2.1.-2.5.

---
+++ part2/course-information/src/components/Content.jsx
@@ -0,0 +1,15 @@
+import Part from './Part'
+import Total from './Total'
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default Content
